8f3702242a29383cf2800bee0982c5e0c09f11b6
Kazem Firouzmandi <kf1375@hotmail.com>
2025-01-19 04:57:10 +0330

    refactor fs_interface to use a class-based structure

--- include/fs_interface.h
+++ include/fs_interface.h
@@ -7,15 +7,18 @@
 #include <mongoose_config.h>
 #include <mongoose.h>
 
-int littlefs_stat(const char *path, size_t *size, time_t *mtime);
-void littlefs_ls(const char *path, void (*fn)(const char *, void *), void *data);
-void *littlefs_op(const char *path, int flags);
-void littlefs_cl(void *fd);
-size_t littlefs_rd(void *fd, void *buf, size_t len);
-size_t littlefs_wr(void *fd, const void *buf, size_t len);
-size_t littlefs_sk(void *fd, size_t offset);
-bool littlefs_mv(const char *from, const char *to);
-bool littlefs_rm(const char *path);
-bool littlefs_mkd(const char *path);
+class FsInterface {
+public:
+    static int littlefs_stat(const char *path, size_t *size, time_t *mtime);
+    static void littlefs_ls(const char *path, void (*fn)(const char *, void *), void *data);
+    static void *littlefs_op(const char *path, int flags);
+    static void littlefs_cl(void *fd);
+    static size_t littlefs_rd(void *fd, void *buf, size_t len);
+    static size_t littlefs_wr(void *fd, const void *buf, size_t len);
+    static size_t littlefs_sk(void *fd, size_t offset);
+    static bool littlefs_mv(const char *from, const char *to);
+    static bool littlefs_rm(const char *path);
+    static bool littlefs_mkd(const char *path);
+};
 
 #endif
--- src/fs_interface.cpp
+++ src/fs_interface.cpp
@@ -1,7 +1,7 @@
 #include "fs_interface.h"
 
 // Function prototypes
-int littlefs_stat(const char *path, size_t *size, time_t *mtime) {
+int FsInterface::littlefs_stat(const char *path, size_t *size, time_t *mtime) {
   File f = LittleFS.open(path, "r");
   if (!f) return 0;
   if (size) *size = f.size();
@@ -10,7 +10,7 @@ int littlefs_stat(const char *path, size_t *size, time_t *mtime) {
   return 1;
 }
 
-void littlefs_ls(const char *path, void (*fn)(const char *, void *), void *data) {
+void FsInterface::littlefs_ls(const char *path, void (*fn)(const char *, void *), void *data) {
   File dir = LittleFS.open(path, "r");
   if (!dir || !dir.isDirectory()) return;
   File file = dir.openNextFile();
@@ -20,7 +20,7 @@ void littlefs_ls(const char *path, void (*fn)(const char *, void *), void *data)
   }
 }
 
-void *littlefs_op(const char *path, int flags) {
+void *FsInterface::littlefs_op(const char *path, int flags) {
   const char *mode = (flags & MG_FS_WRITE) ? "w" : "r";
   File *f = new File(LittleFS.open(path, mode));
   if (!f->available()) {
@@ -30,7 +30,7 @@ void *littlefs_op(const char *path, int flags) {
   return (void *) f;
 }
 
-void littlefs_cl(void *fd) {
+void FsInterface::littlefs_cl(void *fd) {
   File *f = (File *) fd;
   if (f) {
     f->close();
@@ -38,29 +38,29 @@ void littlefs_cl(void *fd) {
   }
 }
 
-size_t littlefs_rd(void *fd, void *buf, size_t len) {
+size_t FsInterface::littlefs_rd(void *fd, void *buf, size_t len) {
   File *f = (File *) fd;
   return f->read((uint8_t *) buf, len);
 }
 
-size_t littlefs_wr(void *fd, const void *buf, size_t len) {
+size_t FsInterface::littlefs_wr(void *fd, const void *buf, size_t len) {
   File *f = (File *) fd;
   return f->write((const uint8_t *) buf, len);
 }
 
-size_t littlefs_sk(void *fd, size_t offset) {
+size_t FsInterface::littlefs_sk(void *fd, size_t offset) {
   File *f = (File *) fd;
   return f->seek(offset);
 }
 
-bool littlefs_mv(const char *from, const char *to) {
+bool FsInterface::littlefs_mv(const char *from, const char *to) {
   return LittleFS.rename(from, to);
 }
 
-bool littlefs_rm(const char *path) {
+bool FsInterface::littlefs_rm(const char *path) {
   return LittleFS.remove(path);
 }
 
-bool littlefs_mkd(const char *path) {
+bool FsInterface::littlefs_mkd(const char *path) {
   return LittleFS.mkdir(path);
 }
